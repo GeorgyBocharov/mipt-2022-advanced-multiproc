1d0a906ba556884244d1e7427ccc445f67107517
Бочаров Георгий Михайлович <gmbocharov@sberbank.ru>
2023-01-07 14:09:37 +0300

    Corrected parallel execution

--- kotlin/src/main/kotlin/bst/TreeMain.kt
+++ kotlin/src/main/kotlin/bst/TreeMain.kt
@@ -4,45 +4,46 @@ import bst.tree.Bst
 import bst.tree.ParallelBst
 import bst.tree.SeqBst
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.runBlocking
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.random.Random
 
 
-val random = Random(13)
-
-val operationsCounter = AtomicInteger(0)
-
-fun main() {
+fun main(): Unit = runBlocking {
     val maxThreads = 4
-    val executor = Executors.newFixedThreadPool(maxThreads)
-    val runWithCheck = true
+    val runWithCheck = false
     val operationExecutor = OperationExecutor()
 
 
-    for (x in arrayOf(50)) {
+    for (x in arrayOf(0)) {
         for (t in 1..maxThreads) {
             val k = IntArray(10) { i -> i + 1 }
-            val resultList = prepopulate(k)
+            val resultList = prepopulate(k, Random(13))
             val parBst = ParallelBst()
-            operationsCounter.set(0)
             val operations = LinkedBlockingQueue<Operation>()
-            val futures: ArrayList<CompletableFuture<Void>> = ArrayList()
-            val end = System.currentTimeMillis() + 5000
+            val executor = Executors.newFixedThreadPool(t)
+            var totalOperations = 0L
+            var maxTime = 0L
+            val list = ArrayList<Deferred<Pair<Long,Long>>>(t)
             for (i in 1..t) {
-                val completableFuture = CompletableFuture.runAsync(
-                    { process(resultList, resultList.size, x, parBst, end, runWithCheck, operations) }, executor)
-                futures.add(completableFuture)
+                list.add(async(executor.asCoroutineDispatcher()) {
+                    process(resultList, resultList.size, x, parBst, runWithCheck, operations)
+                })
             }
-            for (f in futures) {
-                f.get()
+            for (e in list) {
+                val (ops, time) = e.await()
+                totalOperations += ops
+                maxTime = if (time > maxTime) {
+                    time
+                } else {
+                    maxTime
+                }
             }
-            println("For x = $x, threadsNum = $t, op/ms = ${operationsCounter.get() / 5000.0}")
-
+            executor.shutdown()
+            println("For x = $x, threadsNum = $t, op/ms = ${totalOperations * 1.0 / maxTime}")
             if (runWithCheck) {
                 val finalOperations = ArrayList<Operation>(operations)
                 val seqBst = SeqBst()
@@ -54,20 +55,24 @@ fun main() {
 
         }
     }
-    executor.shutdown()
 }
 
-fun process(resultList: IntArray,
-         elementsNumber: Int,
-         x: Int,
-         tree: Bst,
-         end: Long,
-         runWithCheck: Boolean,
-         operationsQueue: LinkedBlockingQueue<Operation>) {
+fun process(
+    resultList: IntArray,
+    elementsNumber: Int,
+    x: Int,
+    tree: Bst,
+    runWithCheck: Boolean,
+    operationsQueue: LinkedBlockingQueue<Operation>
+): Pair<Long, Long> {
+    println("Running in thread ${Thread.currentThread().id}")
+    val random = Random(13)
+    var counter = 0L
+    val end = System.currentTimeMillis() + 5_000
     while (System.currentTimeMillis() < end) {
         val key = resultList[random.nextInt(elementsNumber)]
         val p = random.nextInt(101)
-        operationsCounter.getAndIncrement()
+        counter++
         if (p < x) {
             if (runWithCheck)
                 operationsQueue.add(Operation(key, OperationType.INSERT))
@@ -82,10 +87,11 @@ fun process(resultList: IntArray,
             tree.contains(key)
         }
     }
+    return Pair(counter, System.currentTimeMillis() - end + 5_000)
 }
 
 
-fun prepopulate(array: IntArray): IntArray {
+fun prepopulate(array: IntArray, random: Random): IntArray {
     val res = ArrayList<Int>()
     for (elem in array) {
         if (random.nextFloat() >= 0.5) {
